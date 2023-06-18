e361dfb9dd20a41bc0cdeb14e9bb9023b0718948
antonfront <toxalitvinenko@gmail.com>
2023-06-18 13:45:17 +0300

    erhgerhgerhb

--- src/components/Admin/AdminPage.js
+++ src/components/Admin/AdminPage.js
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form"
 import "./AdminPage.scss"
-
+import { useEffect } from "react"
+import bookApi from "../../services/bookApi"
 const AdminPage = () => {
     const {
         register,
@@ -8,14 +9,15 @@ const AdminPage = () => {
         handleSubmit,
     } = useForm("")
 
-    const onSubmit = handleSubmit((data) => {})
+    const onSubmit = handleSubmit((data) => {
+        alert(JSON.stringify(data))
+    })
     // useEffect(() => {
     //     fetch(
     //         "https://books-a2888-default-rtdb.firebaseio.com/book/assort.json",
     //         {
     //             method: "POST",
     //             body: JSON.stringify({
-    //                 id: 20,
     //                 name: "test",
     //                 img: "test.png",
     //                 author: "test",
@@ -26,11 +28,90 @@ const AdminPage = () => {
     //         }
     //     )
     // }, [])
+    const { data } = bookApi.useFetchBookQuery("")
+
     return (
         <div className="adminka">
             <h1>Адмін панель</h1>
 
-            <form onSubmit={onSubmit}></form>
+            <form onSubmit={onSubmit}>
+                <label>
+                    Назва книги:
+                    <input
+                        {...register("name", {
+                            required: "Це поле обов'язкове для заповнення",
+                        })}
+                    />
+                    {errors?.name && <p>{errors?.name?.message}</p>}
+                </label>
+
+                <label>
+                    Жанри:
+                    <input
+                        type="checkbox"
+                        placeholder="zanr1"
+                        {...register("zanr", {})}
+                    />
+                    <input
+                        type="checkbox"
+                        placeholder="zanr2"
+                        {...register("zanr", {})}
+                    />
+                    <input
+                        type="checkbox"
+                        placeholder="zanr3"
+                        {...register("zanr", {})}
+                    />
+                    <input
+                        type="checkbox"
+                        placeholder="zanr4"
+                        {...register("zanr", {})}
+                    />
+                    {errors?.zanr && <p>{errors?.zanr?.message}</p>}
+                </label>
+
+                <label>
+                    Анотація:
+                    <input
+                        {...register("anotation", {
+                            required: "Це поле обов'язкове для заповнення",
+                        })}
+                    />
+                    {errors?.anotation && <p>{errors?.anotation?.message}</p>}
+                </label>
+
+                <label>
+                    Ціна за м'яку обкладинку:
+                    <input
+                        {...register("cost1", {
+                            required: "Це поле обов'язкове для заповнення",
+                        })}
+                    />
+                    {errors?.cost1 && <p>{errors?.cost1?.message}</p>}
+                </label>
+
+                <label>
+                    Ціна за тверду обкладинку:
+                    <input
+                        {...register("cost2", {
+                            required: "Це поле обов'язкове для заповнення",
+                        })}
+                    />
+                    {errors?.cost2 && <p>{errors?.cost2?.message}</p>}
+                </label>
+
+                <label>
+                    Автор:
+                    <input
+                        {...register("author", {
+                            required: "Це поле обов'язкове для заповнення",
+                        })}
+                    />
+                    {errors?.author && <p>{errors?.author?.message}</p>}
+                </label>
+
+                <input type="submit" value="Додати книгу" />
+            </form>
         </div>
     )
 }
