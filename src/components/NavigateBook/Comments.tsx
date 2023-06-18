846920a5a07fa4ff4f6bf8e799e1b16e896cc856
antonfront <toxalitvinenko@gmail.com>
2023-06-18 12:43:06 +0300

    final

--- src/components/NavigateBook/Comments.tsx
+++ src/components/NavigateBook/Comments.tsx
@@ -4,15 +4,18 @@ import bookApi from "../../services/bookApi"
 import Comment from "./Comment"
 import { useAuth } from "../../hooks/use-auth"
 import { useEffect, useState } from "react"
+import { useAppDispatch } from "../../hooks/redux"
+import { setModalAuthShowed } from "../../store/reducers/user-slice"
 
 const Comments = () => {
     const { id } = useParams()
-
+    const dispatch = useAppDispatch()
     const { data } = bookApi.useFetchBookQuery("")
     const { isAuth, email } = useAuth()
     const [textAreaText, setTextAreaText] = useState("")
     const [obnov, setObnov] = useState(false)
     const [thisComments, setThisComments] = useState<any>([])
+    const [isValid, setIsValid] = useState<boolean>(true)
 
     useEffect(() => {
         fetch("https://books-a2888-default-rtdb.firebaseio.com/comments.json")
@@ -25,19 +28,38 @@ const Comments = () => {
     }, [obnov])
 
     const fetchCommentHandler = () => {
-        fetch("https://books-a2888-default-rtdb.firebaseio.com/comments.json", {
-            method: "POST",
-            body: JSON.stringify({ id, email, comment: textAreaText }),
-        }).then(() => setObnov((prev) => !prev))
+        if (textAreaText.length > 0 && isAuth) {
+            fetch(
+                "https://books-a2888-default-rtdb.firebaseio.com/comments.json",
+                {
+                    method: "POST",
+                    body: JSON.stringify({ id, email, comment: textAreaText }),
+                }
+            ).then(() => {
+                setTextAreaText("")
+                setObnov((prev) => !prev)
+            })
+        } else if (textAreaText.length === 0 && isAuth) {
+            setIsValid(false)
+        } else {
+            dispatch(setModalAuthShowed(true))
+        }
     }
 
     return (
         <div className={styles.comments}>
-            <textarea
-                placeholder="Залиште відгук"
-                onChange={(event) => setTextAreaText(event?.target.value)}
-            ></textarea>
-            <button onClick={fetchCommentHandler}>Відправити</button>
+            <div className={styles.block}>
+                <textarea
+                    value={textAreaText}
+                    className={`${!isValid ? styles.not : ""}`}
+                    placeholder="Залиште відгук"
+                    onChange={(event) => {
+                        setTextAreaText(event?.target.value)
+                        setIsValid(true)
+                    }}
+                ></textarea>
+                <button onClick={fetchCommentHandler}>Відправити</button>
+            </div>
 
             <div className={styles.allComments}>
                 {thisComments.map((item: any, index: number) => (
